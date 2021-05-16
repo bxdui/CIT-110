cd397035acf30b9b069577385a073be135649834
Steven <68931095+bxdui@users.noreply.github.com>
2021-05-16 14:24:31 -0400

    Initial commit

---
+++ Exercise1-5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BigLetter
+{
+    class BigLetter
+    {
+
+        static void Main()
+        {
+            BigLetter.NormalLine(2);
+            Console.WriteLine("HHHHHHHH");
+            BigLetter.NormalLine(3);
+        }
+
+        static void NormalLine(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("H      H");
+            }
+        }
+    }
+}
