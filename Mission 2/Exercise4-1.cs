e65e71669192a87a10fb6702f6dbb8e9a994c21c
Steven <68931095+bxdui@users.noreply.github.com>
2021-05-30 16:13:56 -0400

    Added Mission 2 exercises

---
+++ Mission 2/Exercise4-1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+// Exercise prompt:
+// Write a program named CheckCredit that prompts users to enter a purchase price for an item.
+// If the value entered is greater than a credit limit of $8,000, display You have exceeded the credit limit; otherwise, display Approved.
+
+class CheckCredit
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the purchase price:");
+        string priceInput = Console.ReadLine();
+        int price;
+
+        while (!Int32.TryParse(priceInput, out price))
+        {
+            Console.WriteLine("Please enter a non-comma integer without $");
+            priceInput = Console.ReadLine();
+        }
+
+
+        if (price > 8000)
+        {
+            Console.WriteLine("You have exceeded the credit limit");
+        }
+        else
+        {
+            Console.WriteLine("Approved");
+        }
+    }
+}
